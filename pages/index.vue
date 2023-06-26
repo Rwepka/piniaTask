07567a48ac6a521e84994f58e5b43c875718dffc
Rwepka <rwepka@gmail.com>
2023-06-26 13:59:34 +0300

    Готово

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-for="num of numStore.nums" :key="num.id">
+    <h3><button @click="num.count ++">Добавить</button> {{ num.count }}</h3>
+  </div>
+</template>
+
+<script setup>
+import { defineStore } from "pinia";
+const useNumStore = defineStore('NumStore', {
+  state(){
+    return {
+      nums: [
+        {
+          id: 1,
+          count: 0
+        },
+        {
+          id: 2,
+          count: 0
+        },
+        {
+          id: 3,
+          count: 0
+        },
+      ]
+    }
+  }
+})
+const numStore = useNumStore()
+</script>
+
+<style>
+
+</style>
