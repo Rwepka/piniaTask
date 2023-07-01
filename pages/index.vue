7dce970a61cb27320a496f14d642127cc0de46a6
Rysakov Maxim <maxim@rysakov.ru>
2023-07-01 11:15:15 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
     <nuxt-link to="/page/1"><h1>Следующее задание</h1></nuxt-link>
+    <!-- TODO: выводится должно с сортировкой по увеличению. 1, 2, 3 и тж -->
     <div v-for="num of numStore.nums" :key="num.id">
-      <h3><button @click="numStore.numPlus(num.id)">Добавить</button> {{ num.count }}</h3>
+      <h3>
+        <button @click="numStore.numPlus(num.id)">Добавить</button>
+        {{ num.id }} - {{ num.count }}
+      </h3>
     </div>
   </div>
 </template>
