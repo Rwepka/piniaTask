e37c1abc40d910b5f85b50aa7dde3d8e139bdd58
Rwepka <rwepka@gmail.com>
2023-07-02 12:34:52 +0300

    Пофиксил

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
+    <Pagination 
+    :posts="posts"
+    />
   </div>
 </template>
-
 <script setup>
   const {data: posts} = await useFetch('https://jsonplaceholder.typicode.com/posts')
 </script>
